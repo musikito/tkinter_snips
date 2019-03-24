52d26d5983f8cf719a63f3176b683262d72bc213
jose marte <tumusicalatina@gmail.com>
2019-03-24 14:42:22 -0400

    added "file" menu

--- TkMp3.py
+++ TkMp3.py
@@ -25,14 +25,58 @@ class Mp3Panel(wx.Panel):
         print('in on_edit')
 
     def update_mp3_listing(self, folder_path):
-        print(folder_path)
+        self.current_folder_path = folder_path
+        self.list_ctrl.ClearAll()
 
-class Mp3Frame(wx.Frame):    
+        self.list_ctrl.InsertColumn(0, 'Artist', width=140)
+        self.list_ctrl.InsertColumn(1, 'Album', width=140)
+        self.list_ctrl.InsertColumn(2, 'Title', width=200)
+        self.list_ctrl.InsertColumn(3, 'Year', width=200)
+
+        mp3s = glob.glob(folder_path + "/*.mp3")
+        print(mp3s)
+        mp3_objects = []
+        index = 0
+        for mp3 in mp3s:
+            mp3_object = eyed3.load(mp3)
+            self.list_ctrl.InsertItem(index, mp3_object.tag.artist)
+            self.list_ctrl.SetItem(index, 1, mp3_object.tag.album)
+            self.list_ctrl.SetItem(index, 2, mp3_object.tag.title)
+            mp3_objects.append(mp3_object)
+            self.row_obj_dict[index] = mp3_object
+            index += 1
+
+class Mp3Frame(wx.Frame):
+
     def __init__(self):
-        super().__init__(parent=None,
-                         title='Mp3 Tag Editor')
+        wx.Frame.__init__(self, parent=None, 
+                          title='Mp3 Tag Editor')
         self.panel = Mp3Panel(self)
+        self.create_menu()
         self.Show()
+
+    def create_menu(self):
+        menu_bar = wx.MenuBar()
+        file_menu = wx.Menu()
+        open_folder_menu_item = file_menu.Append(
+            wx.ID_ANY, 'Open Folder', 
+            'Open a folder with MP3s'
+        )
+        menu_bar.Append(file_menu, '&File')
+        self.Bind(
+            event=wx.EVT_MENU, 
+            handler=self.on_open_folder,
+            source=open_folder_menu_item,
+        )
+        self.SetMenuBar(menu_bar)
+
+    def on_open_folder(self, event):
+        title = "Choose a directory:"
+        dlg = wx.DirDialog(self, title, 
+                           style=wx.DD_DEFAULT_STYLE)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.panel.update_mp3_listing(dlg.GetPath())
+        dlg.Destroy()
 
 if __name__ == '__main__':
     app = wx.App(False)
