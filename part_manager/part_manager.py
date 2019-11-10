68c2cebdd552402ec81ddf4516fc0efdc33d58fd
JoseMiguel Marte <tumusicalatina@gmail.com>
2019-11-10 11:04:58 -0500

    Added the DB file

--- part_manager/part_manager.py
+++ part_manager/part_manager.py
@@ -1,14 +1,37 @@
 from tkinter import *
 
+# Import the DB class
+from db import Database
+db = Database("store.db")
+
 # create window object
 app = Tk()
 
-# change the title
+# functions section
+def populate_list():
+    part_list.delete(0, END)
+    for row in db.fetch():
+        part_list.insert(END,row)
+
+def add_item():
+    print("Add")
+
+def remove_item():
+    print("remove")
+
+def update_item():
+    print("Update")
+
+def clear_text():
+    print("Clear")
+
+# Title
 app.title("Part Manager")
 
 # Size
 app.geometry("700x350")
 
+# Part
 part_text = StringVar()
 part_label = Label(app, text='Part Name', font=('bold',14), pady=20)
 # Place the label on the 'grid'
@@ -17,6 +40,64 @@ part_label.grid(row=0, column=0, sticky =W)
 part_entry = Entry(app, textvariable=part_text)
 # part_entry.insert(0,"placeholder")
 part_entry.grid(row=0,column=1)
+
+# Customer
+customer_text = StringVar()
+customer_label = Label(app, text='Customer', font=('bold',14))
+# Place the label on the 'grid'
+customer_label.grid(row=0, column=2, sticky =W)
+# Customer entry
+customer_entry = Entry(app, textvariable=customer_text)
+# customer_entry.insert(0,"placeholder")
+customer_entry.grid(row=0,column=3)
+
+# Retailer
+retailer_text = StringVar()
+retailer_label = Label(app, text='Retailer', font=('bold',14))
+# Place the label on the 'grid'
+retailer_label.grid(row=1, column=0, sticky =W)
+# Retailer entry
+retailer_entry = Entry(app, textvariable=retailer_text)
+# retailer_entry.insert(0,"placeholder")
+retailer_entry.grid(row=1,column=1)
+
+# Price
+price_text = StringVar()
+price_label = Label(app, text='Price', font=('bold',14))
+# Place the label on the 'grid'
+price_label.grid(row=1, column=2, sticky =W)
+# Price entry
+price_entry = Entry(app, textvariable=price_text)
+# price_entry.insert(0,"placeholder")
+price_entry.grid(row=1,column=3)
+
+
+# Parts list(listbox widget)
+part_list = Listbox(app,height=8, width=50) #, border =0)
+part_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
+# create the scrollbar
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=3, column=3)
+# stick/attach to the scrollbar
+part_list.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=part_list.yview)
+
+# Buttons Section
+# Add button
+add_btn = Button(app, text="Add Part",width=12, command=add_item)
+add_btn.grid(row=2, column=0, pady=20)
+# Remove button
+remove_btn = Button(app, text="Remove Part",width=12, command=remove_item)
+remove_btn.grid(row=2, column=1)
+# Update button
+update_btn = Button(app, text="Update Part",width=12, command=update_item)
+update_btn.grid(row=2, column=2)
+# Clear button
+clear_btn = Button(app, text="Clear Input",width=12, command=clear_text)
+clear_btn.grid(row=2, column=3)
+
+# Populate the parts list when we open the app
+populate_list()
 
 # Start the program
 app.mainloop()
